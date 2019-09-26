9779e0fb63b39b30e30d931823868c8708ee3461
Dqhan <291927006@qq.com>
2019-09-26 17:57:42 +0800

    setState

--- React之setState.js
+++ React之setState.js
@@ -155,7 +155,333 @@ var ReactUpdates = {
             batchingStrategy.batchedUpdates(enqueueUpdate, component);
             return;
         }
-        
-        dirtyComponents.push(component);
+
+        dirtyComponents.push(component);   //component 就是internalInstance  即 component在react内部的实例表达
     }
 }
+
+/**
+ * 如果batchingStrategy.isBatchingUpdates的值是true，表示当前正在处于批量更新中，直接讲component加入队列，
+ * 否则执行 batchingStrategy.batchedUpdates
+ * 那么 batchingStrategy.batchedUpdates 干了什么呢
+ */
+
+var ReactDefaultBatchingStrategy = {
+    isBatchingUpdates: false,
+    batchedUpdates: function (callback, a, b, c, d, e) {
+        var alreadyBatchUpdates = ReactDefaultBatchingStrategy.isBatchingUpdates;
+        ReactDefaultBatchingStrategy.isBatchingUpdates = true;
+        if (alreadyBatchUpdates) {
+            callback(a, b, c, d, e);
+        } else {
+            transaction.perform(callback, null, a, b, c, d, e);
+        }
+    }
+}
+
+/**
+ * isBatchingUpdates 表示是否处于批量更新中
+ */
+
+/**
+ * transaction 事务
+ */
+
+/**
+* <pre>
+*                       wrappers (injected at creation time)
+*                                      +        +
+*                                      |        |
+*                    +-----------------|--------|--------------+
+*                    |                 v        |              |
+*                    |      +---------------+   |              |
+*                    |   +--|    wrapper1   |---|----+         |
+*                    |   |  +---------------+   v    |         |
+*                    |   |          +-------------+  |         |
+*                    |   |     +----|   wrapper2  |--------+   |
+*                    |   |     |    +-------------+  |     |   |
+*                    |   |     |                     |     |   |
+*                    |   v     v                     v     v   | wrapper
+*                    | +---+ +---+   +---------+   +---+ +---+ | invariants
+* perform(anyMethod) | |   | |   |   |         |   |   | |   | | maintained
+* +----------------->|-|---|-|---|-->|anyMethod|---|---|-|---|-|-------->
+*                    | |   | |   |   |         |   |   | |   | |
+*                    | |   | |   |   |         |   |   | |   | |
+*                    | |   | |   |   |         |   |   | |   | |
+*                    | +---+ +---+   +---------+   +---+ +---+ |
+*                    |  initialize                    close    |
+*                    +-----------------------------------------+
+* </pre>
+* 
+* React transaction会把方法封装成一个一个的wrapper， 每个wrapper都有两个方法，initialize 与 close
+* 当方法执行的时候 首先需要执行以下transaction 的perform 然后执行wrapper.initialize 然后执行方法本身，然后执行wrapper.close
+* 过程： transaction.proform  =>   wrapper.initialize  => fn => wrapper.close 
+*/
+
+var RESET_BATCHED_UPDATES = {
+    initialize: emptyFunction,
+    colse: function () {
+        ReactDefaultBatchingStrategy.isBatchingUpdates = false;
+    }
+}
+
+var FLUSH_BATCHED_UPDATES = {
+    initialize: emptyFunction,
+    close: ReactUpdates.flushBatchedUpdates.bind(ReactUpdates), //flushBatchedUpdates   virtual dom 与 真实dom的映射
+}
+
+var TRANSACTION_WRAPPERS = [FLUSH_BATCHED_UPDATES, RESET_BATCHED_UPDATES];
+
+
+function ReactDefaultBatchingStrategyTransaction() {
+    this.reinitializeTransaction();
+}
+
+Object.assign(
+    ReactDefaultBatchingStrategyTransaction.prototype,
+    Transaction.Mixin,
+    {
+        getTransactionWrappers: function () {
+            return TRANSACTION_WRAPPERS;
+        },
+    }
+);
+
+var transaction = new ReactDefaultBatchingStrategyTransaction();
+
+/**
+ * 一个transaction 包含一个wrapper数组，这个wrapper数组包含两个wrapper，RESET_BATHED_UPDATES跟FLUSH_BATCHED_UPDATES
+ * transaction => wrapper =>{
+ *                                 1,RESET_BATCHED_UPDATES
+ *                                 2,FLUSH_BATCHED_UPDATES
+ *                          }
+ */
+
+/**
+ *  回顾 setState 更新过程
+ *  如果处于批量跟新过程中（isBatchingUpdates == true）直接将component 加入dirtyComponents队列中
+ *  如果不是，开启批量更新 => enqueueUpdata
+ *  开启批量更新的过程 transaction.perform => wrapper.initialize => enqueueUpdata => wrapper.close  => (isBatchingUpdates == false)
+ *  component 加入 dirtyComponents 之后发生了什么呢
+ */
+
+/**
+ * batchedUpdates内执行了一个事务，该事务处于close阶段做了两件事
+ * 1  ReactDefaultBatchingStrategy.isBatchingUpdates 设置成false 表示关闭批量更新
+ * 2  调用ReactUpdates.flushBatchedUpdates   ， flushBatchedUpdates中涉及virtual dom 与真实dom的映射
+ */
+
+var flushBatchedUpdates = function () {
+    while (dirtyComponents.length) {
+        if (dirtyComponents.length) {
+            var transaction = ReactUpdatesFlushTransaction.getPooled();
+            transaction.perform(runBatchedUpdates, null, transaction);
+            ReactUpdatesFlushTransaction.release(transaction);
+        }
+        //......
+    }
+};
+
+/**
+ * ReactUpdatesFlushTransaction中的wrapper定义
+ */
+
+var UPDATE_QUEUEING = {
+    initialize: function() {
+      this.callbackQueue.reset();
+    },
+    close: function() {
+      this.callbackQueue.notifyAll();
+    },
+  };
+
+/**
+ * 会发现flushBatchedUpdates中运行了一个事务，函数主体runBatchedUpdates
+ * runBatchedUpdates做了什么事儿
+ */
+
+function runBatchedUpdates(transaction) {
+    var len = transaction.dirtyComponentsLength;
+    dirtyComponents.sort(mountOrderComparator);
+
+    for (var i = 0; i < len; i++) {
+        var component = dirtyComponents[i];
+        var callbacks = component._pendingCallbacks;
+        component._pendingCallbacks = null;
+        //.....
+        ReactReconciler.performUpdateIfNecessary(component, transaction.reconcileTransaction);
+        //.......
+        if (callbacks) {
+            for (var j = 0; j < callbacks.length; j++) {
+                transaction.callbackQueue.enqueue(
+                    callbacks[j],
+                    component.getPublicInstance()
+                );
+            }
+        }
+    }
+}
+
+/**
+ * 执行setState的回调函数，执行ReactReconciler.performUpdateIfNecessary
+ * ReactReconciler.performUpdateIfNecessary 干了什么
+ */
+
+function performUpdateIfNecessary(internalInstance, transaction) {
+    internalInstance.performUpdateIfNecessary(transaction);
+}
+var ReactCompositeComponentMixin = {
+    performUpdateIfNecessary: function (transaction) {
+        //......
+        if (this._pendingStateQueue !== null || this._pendingForceUpdate) {
+            this.updateComponent(
+                transaction,
+                this._currentElement,
+                this._currentElement,
+                this._context,
+                this._context
+            );
+        }
+    }
+}
+/**
+ * 上面代码是perfromUpdateIfNecessary的省略版本，主要调用的其中的this.updateComponent方法:
+ */
+function updateComponent(
+    transaction,
+    prevParentElement,
+    nextParentElement,
+    prevUnmaskedContext,
+    nextUnmaskedContext
+) {
+    var inst = this._instance;
+    var willReceive = false;
+    var nextContext;
+    var nextProps;
+
+    // 验证组件context是否改变
+    // ......
+
+    // 验证是否是props更新还是组件state更新
+    if (prevParentElement === nextParentElement) {
+        nextProps = nextParentElement.props;
+    } else {
+        //存在props的更新  
+        nextProps = this._processProps(nextParentElement.props);
+        willReceive = true;
+    }
+    //根据条件判断是否调用钩子函数componentWillReceiveProps
+    if (willReceive && inst.componentWillReceiveProps) {
+        inst.componentWillReceiveProps(nextProps, nextContext);
+    }
+    //计算新的state
+    var nextState = this._processPendingState(nextProps, nextContext);
+
+    var shouldUpdate =
+        this._pendingForceUpdate ||
+        !inst.shouldComponentUpdate ||
+        inst.shouldComponentUpdate(nextProps, nextState, nextContext);
+
+    if (shouldUpdate) {
+        this._pendingForceUpdate = false;
+        this._performComponentUpdate(
+            nextParentElement,
+            nextProps,
+            nextState,
+            nextContext,
+            transaction,
+            nextUnmaskedContext
+        );
+    } else {
+        this._currentElement = nextParentElement;
+        this._context = nextUnmaskedContext;
+        inst.props = nextProps;
+        inst.state = nextState;
+        inst.context = nextContext;
+    }
+}
+
+/**
+ * 计算state
+ */
+
+function _processPendingState(props, context) {
+    var inst = this._instance;
+    var queue = this._pendingStateQueue;
+    var replace = this._pendingReplaceState;
+    this._pendingReplaceState = false;
+    this._pendingStateQueue = null;
+
+    if (!queue) {
+      return inst.state;
+    }
+
+    if (replace && queue.length === 1) {
+      return queue[0];
+    }
+
+    var nextState = Object.assign({}, replace ? queue[0] : inst.state);
+    for (var i = replace ? 1 : 0; i < queue.length; i++) {
+      var partial = queue[i];
+      Object.assign(
+        nextState,
+        typeof partial === 'function' ?
+          partial.call(inst, nextState, props, context) :
+          partial
+      );
+    }
+
+    return nextState;
+  }
+}
+
+//如果我们传入对象是
+this.setState({
+    value: this.state.value + 1
+})
+
+this.setState({
+    value:this.state.value +1 
+})
+
+function _performComponentUpdate(
+    nextElement,
+    nextProps,
+    nextState,
+    nextContext,
+    transaction,
+    unmaskedContext
+  ) {
+    var inst = this._instance;
+
+    var hasComponentDidUpdate = Boolean(inst.componentDidUpdate);
+    var prevProps;
+    var prevState;
+    var prevContext;
+    if (hasComponentDidUpdate) {
+      prevProps = inst.props;
+      prevState = inst.state;
+      prevContext = inst.context;
+    }
+
+    if (inst.componentWillUpdate) {
+      inst.componentWillUpdate(nextProps, nextState, nextContext);
+    }
+
+    this._currentElement = nextElement;
+    this._context = unmaskedContext;
+    inst.props = nextProps;
+    inst.state = nextState;
+    inst.context = nextContext;
+
+    this._updateRenderedComponent(transaction, unmaskedContext);
+
+    if (hasComponentDidUpdate) {
+      transaction.getReactMountReady().enqueue(
+        inst.componentDidUpdate.bind(inst, prevProps, prevState, prevContext),
+        inst
+      );
+    }
+}
+
+
